763f2d8ff8713d7ccb246eb22a2b22e06dd314e8
JasonMcGee0715 <jdm0715@gmail.com>
2020-05-09 22:25:20 -0500

    finito

--- main.js
+++ main.js
@@ -11,23 +11,55 @@ const rl = readline.createInterface({
 });
 
 
-const pigLatin = (oldword) => {
-  let vowel = ["a", "e", "i", "o", "u"]
-  for (let i = 0 ; i < vowel.length ; i++) {
-    if (oldword.charAt(0) == vowel[i]){
-      console.log(oldword+"yay");
-    }
-    else if (oldword.charAt(1) == vowel[i] && oldword.charAt(0)) {
-      return oldword.splice(1) + oldword.charAt(0) + "ay";
-    }
-    else if (oldword.charAt(2) == vowel[i] && oldword.charAt(0)) {
-      return oldword.splice(2) + oldword.charAt(0) + oldword.charAt(1) + "ay";
-    }
-    else if (oldword.charAt(3) == vowel[i] && oldword.charAt(0)) {
-      return oldword.splice(3) + oldword.charAt(0) + oldword.charAt(1) + oldword.charAt(2) + "ay";
-    }
+// const pigLatin = (oldword) => {
+//   let vowel = ["a", "e", "i", "o", "u"]
+//   for (let i = 0 ; i < vowel.length ; i++) {
+//     if (oldword.charAt(0) == vowel[i]){
+//       console.log(oldword+"yay");
+//     }
+//     else if (oldword.charAt(1) == vowel[i] && oldword.charAt(0)) {
+//       return oldword.splice(1) + oldword.charAt(0) + "ay";
+//     }
+//     else if (oldword.charAt(2) == vowel[i] && oldword.charAt(0)) {
+//       return oldword.splice(2) + oldword.charAt(0) + oldword.charAt(1) + "ay";
+//     }
+//     else if (oldword.charAt(3) == vowel[i] && oldword.charAt(0)) {
+//       return oldword.splice(3) + oldword.charAt(0) + oldword.charAt(1) + oldword.charAt(2) + "ay";
+//     }
+//   }
+// }
+
+const pigLatin = (oldword)=>{
+  if (oldword.match(/ /)) {
+    oldword = oldword.toLowerCase().trim();
+  let wordArr = oldword.split(' ');
+   // console.log(wordArr)
+  let result = '';
+  for (let i = 0; i < wordArr.length; i++) {
+  if (i > 0) {
+  result += ' ';
   }
-}
+  result += translate(wordArr[i]);
+  }
+  return result;
+  }
+  return translate(oldword);
+  }
+  const translate = (oldword) =>{
+  let vowels = ["a", "e", "i", "o", "u"]
+  let newWord = "";
+  if (vowels.indexOf(oldword[0]) > -1){
+  newWord = oldword + "yay";
+  return newWord;
+  }
+  
+  else{
+  let firstLetter = oldword.match(/[aeiou]/);
+  let vowel = oldword.indexOf(firstLetter[0]);
+  newWord = oldword.substring(vowel) + oldword.substring(0, vowel) + "ay";
+  return newWord;
+  }
+  }
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
